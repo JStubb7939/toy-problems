2ad97f506c1899f9600c836ca9af069b6790c0ad
Admin <admin@Jordans-MacBook-Pro.local>
2017-05-27 16:52:14 -0500

    Add mocha test script to package json

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+var toyProblems = require('../index.js');
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
