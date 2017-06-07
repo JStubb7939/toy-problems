2d345557b4a7cdfbe9475d0b26446373443b90a8
Jordan Stubblefield <jstubblefield7939@gmail.com>
2017-06-06 20:11:20 -0500

    write test cases for sumOfTwo

--- problems/sumOfTwo/sumOfTwo.test.js
+++ problems/sumOfTwo/sumOfTwo.test.js
@@ -4,7 +4,21 @@ var sumOfTwo = require('./sumOfTwo');
 
 
 describe('sumOfTwo', function() {
-  it('should return ["1010", "1011"] when given "101?"', function() {
-    
+  it('should return true when given ([1,2,3], [10,20,30,40], 42)', function() {
+    expect(sumOfTwo([1,2,3], [10,20,30,40], 42)).to.equal(true);
+  });
+
+  it('should return false when given ([1,2,3], [10,20,30], 42)', function() {
+    expect(sumOfTwo([1,2,3], [10,20,30], 42)).to.equal(false);
+  });
+
+  it('should return false when one/both of the arrays is/are empty and a value is given', function() {
+    expect(sumOfTwo([], [10,20,30], 42)).to.equal(false);
+    expect(sumOfTwo([1,2,3], [], 42)).to.equal(false);
+    expect(sumOfTwo([], [], 42)).to.equal(false);
+  });
+
+  it('should return true when both of the arrays are empty and a value is given', function() {
+    expect(sumOfTwo([], [])).to.equal(true);
   });
 });
