aaca23bff4932ebd9f6701a7c02b7cee6b98b37c
Jordan Stubblefield <jstubblefield7939@gmail.com>
2017-06-06 20:00:58 -0500

    set up sumOfTwo

---
+++ problems/sumOfTwo/sumOfTwo.test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var sumOfTwo = require('./sumOfTwo');
+
+
+describe('sumOfTwo', function() {
+  it('should return ["1010", "1011"] when given "101?"', function() {
+  });
+});
