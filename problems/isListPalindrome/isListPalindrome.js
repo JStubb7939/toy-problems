d1ae281d8e04665301188453a9dea772b3948034
Jordan Stubblefield <jstubblefield7939@gmail.com>
2017-06-14 21:58:29 -0400

    set up splitEvenNumbers

--- problems/isListPalindrome/isListPalindrome.js
+++ problems/isListPalindrome/isListPalindrome.js
@@ -1,4 +1,5 @@
-/*Note: Try to solve this task in O(n) time using O(1) additional space, where n is the number of elements in l, since this is what you'll be asked to do during an interview.
+/*
+Note: Try to solve this task in O(n) time using O(1) additional space, where n is the number of elements in l, since this is what you'll be asked to do during an interview.
 
 Given a singly linked list of integers, determine whether or not it's a palindrome.
 
